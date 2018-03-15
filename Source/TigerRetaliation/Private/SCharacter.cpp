7beae849701b560eb32db6d88b83937d96b12ad4
wholesalebirbs <36047288+wholesalebirbs@users.noreply.github.com>
2018-03-15 16:50:06 -0700

    Setup AI Pawn and Navigation Mesh

--- Source/TigerRetaliation/Private/SCharacter.cpp
+++ Source/TigerRetaliation/Private/SCharacter.cpp
@@ -179,5 +179,6 @@ void ASCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifet
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME(ASCharacter, CurrentWeapon);
+	DOREPLIFETIME(ASCharacter, bDied);
 }
 
